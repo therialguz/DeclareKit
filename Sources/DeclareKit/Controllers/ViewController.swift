768447f95470e220274dfc98b9d82d6099f8891b
Benjamín Guzmán <benjamingl99@gmail.com>
2026-02-09 19:05:23 -0300

    Move view generations to loadView

--- Sources/DeclareKit/Controllers/ViewController.swift
+++ Sources/DeclareKit/Controllers/ViewController.swift
@@ -41,10 +41,10 @@ final class HostViewController<Content: RepresentableNode>: UIViewController, Li
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    override func loadView() {
+        super.loadView()
+        
         let views = content().buildList()
         for child in views {
             view.addSubview(child)
@@ -55,7 +55,10 @@ final class HostViewController<Content: RepresentableNode>: UIViewController, Li
                 child.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             ])
         }
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
         lifecycleCallbacks.viewDidLoad?()
     }
 
